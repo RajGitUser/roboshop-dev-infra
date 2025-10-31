1373a36e6ea8b125691e428d1041b580a7514b1e
Rajkumar <rajkumarmanda@outlook.com>
2025-10-31 13:07:15 +0530

    commit

--- 30-sg-rules/data.tf
+++ 30-sg-rules/data.tf
@@ -21,3 +21,7 @@ data "aws_ssm_parameter" "bastion_sg_id" {
 data "aws_ssm_parameter" "private_subnet_ids" {
   name  = "/${var.project_name}/${var.environment}/private_subnet_ids"
 }
+
+data "aws_ssm_parameter" "mongodb_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/mongodb_sg_id"
+}
