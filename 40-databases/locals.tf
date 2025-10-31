655245edec47c880aea04ffdc95a5b5291b0a012
Rajkumar <rajkumarmanda@outlook.com>
2025-10-31 13:15:25 +0530

    commit

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -2,7 +2,6 @@ locals {
     ami_id = data.aws_ami.joindevops.id
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.vlaue
     database_subnet_id = split("," , data.aws_ssm_parameter.database_subnet_ids.value)[0]
-    ami_id = data.aws_ami.joindevops.id
     common_tags = {
         project_name = var.project_name
         environment = var.environment
