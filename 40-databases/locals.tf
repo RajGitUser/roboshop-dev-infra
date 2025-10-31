cbfb9bb9534b6ed13f7e5b2231ca2c3eb5c9cab3
Rajkumar <rajkumarmanda@outlook.com>
2025-10-31 12:58:18 +0530

    Initial clean commit (removed large files)

---
+++ 40-databases/locals.tf
@@ -0,0 +1,10 @@
+locals {
+    ami_id = data.aws_ami.joindevops.id
+    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.vlaue
+    database_subnet_id = split("," , data.aws_ssm_parameter.database_subnet_ids.value)[0]
+    common_tags = {
+        project_name = var.project_name
+        environment = var.environment
+        terraform = true
+    }
+}
