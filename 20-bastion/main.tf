92ea6afa7871da3ee89c8e873c3562319cd68d93
Rajkumar <rajkumarmanda@outlook.com>
2025-11-05 12:54:40 +0530

    commit

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "bastion" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id = local.public_subnet_id
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
 
   tags = merge (
     local.common_tags,
@@ -10,4 +11,9 @@ resource "aws_instance" "bastion" {
         Name = "${var.project_name}-${var.environment}-bastion"
     }
   )
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "bastion"
+  role = "BastionTerraformAdmin"
 }
