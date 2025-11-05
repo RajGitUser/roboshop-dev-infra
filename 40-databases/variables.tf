f4ef4375d2f6ab5b5d38c3964b0d560548e1f866
Rajkumar <rajkumarmanda@outlook.com>
2025-11-05 14:00:24 +0530

    commit

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -6,6 +6,28 @@ variable "environment" {
     default = "dev"
 }
 
-variable "bastion_tags" {
-    default = {}
+variable "sg_names" {
+    default = [
+        # databases
+        "mongodb", "redis", "mysql", "rabbitmq",
+        # backend
+        "catalogue", "user", "cart", "shipping", "payment",
+        # frontend
+        "frontend",
+        # bastion
+        "bastion",
+        # frontend load balancer
+        "frontend_lb",
+        # backend ALB
+        "backend_alb"
+        
+    ]
+}
+
+variable "zone_id" {
+    default = "Z08088429XZJJZMETS6D"
+}
+
+variable "domaine_name" {
+    default = "rajkumardaws.space" #rajkumardaws.space
 }
