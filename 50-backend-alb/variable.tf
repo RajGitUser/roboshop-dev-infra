1d648af2b0e1a873e0effa341a43c30bbba87f0b
Rajkumar <rajkumarmanda@outlook.com>
2025-11-06 13:56:28 +0530

    commit

--- 50-backend-alb/variable.tf
+++ 50-backend-alb/variable.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z08088429XZJJZMETS6D"
+}
+
+variable "domain_name" {
+    default = "rajkumardaws.space"
+}
