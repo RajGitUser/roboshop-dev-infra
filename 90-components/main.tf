b2424aabbca6c54320af32290ca39d8a4d507160
Rajkumar <rajkumarmanda@outlook.com>
2025-11-13 15:05:24 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,3 +1,9 @@
+# module "components" {
+#     source = "https://github.com/RajGitUser/terraform-roboshop-component.git?ref=main"
+#     component = var.component
+#     rule_priority = var.rule_priority
+# }
+
 module "components" {
     source = "../../terraform-roboshop-component"
     component = var.component
