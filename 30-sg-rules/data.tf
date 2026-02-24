cbe93a77d8308d82dab3c5319d5609c3bccdd841
Rajkumar <rajkumarmanda@outlook.com>
2026-02-23 23:53:41 -0500

    jenkins

--- 30-sg-rules/data.tf
+++ 30-sg-rules/data.tf
@@ -22,9 +22,9 @@ data "aws_ssm_parameter" "bastion_sg_id" {
   name  = "/${var.project_name}/${var.environment}/bastion_sg_id"
 }
 
-data "aws_ssm_parameter" "private_subnet_ids" {
-  name  = "/${var.project_name}/${var.environment}/private_subnet_ids"
-}
+# data "aws_ssm_parameter" "private_subnet_ids" {
+#   name  = "/${var.project_name}/${var.environment}/private_subnet_ids"
+# }
 
 data "aws_ssm_parameter" "mongodb_sg_id" {
   name  = "/${var.project_name}/${var.environment}/mongodb_sg_id"
